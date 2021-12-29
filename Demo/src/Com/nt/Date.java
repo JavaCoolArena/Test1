fd44d958c8203db0fa322dc7d71c3bbfaa5f9084
Dev1 <Dev1@gmail.com>
2021-12-29 15:39:32 +0530

    release stashes

--- Demo/src/Com/nt/Date.java
+++ Demo/src/Com/nt/Date.java
@@ -6,8 +6,13 @@ public class Date {
 		java.util.Date d=new java.util.Date();
 		System.out.println(d.toLocaleString());
 		System.out.println("wel come");
+		m2();
 	}
 	
 	
+	public  static void m2() {
+		System.out.println("Return stashes");
+	}
+
 
 }
